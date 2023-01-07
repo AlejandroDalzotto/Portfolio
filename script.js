41a5bf332ee846c007fb057046e0c5f39d308733
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-01-06 21:44:54 -0300

    navbar se actualiza la pasar por cada article

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+/**
+ * Habilitar y desabilitar la navbar
+ */
 
 const btn = document.getElementById('btn')
-const nav = document.getElementById('nav')
+const navbar = document.getElementById('nav')
 
 btn.addEventListener('click', displaceNavbar)
-nav.addEventListener('touchend', displaceNavbar)
+navbar.addEventListener('touchend', displaceNavbar)
 
 function displaceNavbar() {
-    nav.classList.toggle('active')
+    navbar.classList.toggle('active')
 }
+
+/**
+ * Segruimiento del menu con el scroll
+ */
+
+const articles = document.querySelectorAll('article');
+const navLinks = document.querySelectorAll('nav a');
+
+const scrollActive = () => {
+    const scrollPos = window.scrollY + navbar.offsetHeight;
+
+    articles.forEach((section, index) => {
+        if (scrollPos > section.offsetTop -50) {
+            navLinks.forEach(link =>
+                link.classList.remove('active-link')
+            );
+            navLinks[index].classList.add('active-link');
+        }
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
